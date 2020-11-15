60d25f45f6ed02307e90e5311ff85044d665a587
CristiDatas <cristidatas@gmail.com>
2020-11-15 16:26:08 +0100

    EDA Covid

---
+++ README.md
@@ -0,0 +1,3 @@
+# Proyecto-Covid_Agosto
+
+ 
---
+++ src/graf.py
@@ -0,0 +1,10 @@
+# Este módulo contiene la función que hace el gráfico del json del equipo A @CristiDatas
+
+import pandas as pd
+from matplotlib import pyplot as plt
+
+def graf_a(graf):
+    pl=pd.read_json(graf)
+    pl.plot()
+    return plt.show()
+graf_a(graf="resources/json/output/grupoa.json")
